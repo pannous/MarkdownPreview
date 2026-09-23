c595e8ad990f61067a526e1c15de563425345e39
pannous <info@pannous.com>
2026-09-23 15:42:12 +0200

    feature(minor): quit when the last window or tab is closed

--- App/MarkdownPreviewApp.swift
+++ App/MarkdownPreviewApp.swift
@@ -15,8 +15,15 @@ struct MarkdownDocument: FileDocument {
     }
 }
 
+/// A viewer has nothing to show once its last window (or tab) is closed, so quit instead of idling in the Dock.
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool { true }
+}
+
 @main
 struct MarkdownPreviewApp: App {
+    @NSApplicationDelegateAdaptor private var appDelegate: AppDelegate
+
     var body: some Scene {
         DocumentGroup(viewing: MarkdownDocument.self) { file in
             if let fileURL = file.fileURL {
